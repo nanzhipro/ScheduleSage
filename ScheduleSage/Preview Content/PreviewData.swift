bd62798fb4c4eaa8945baed76ec53b7e0130790f
南知 <deer_hope@live.cn>
2025-02-01 14:40:08 +0800

    Delete unused code

--- ScheduleSage/Preview Content/PreviewData.swift
+++ ScheduleSage/Preview Content/PreviewData.swift
@@ -1,33 +1,7 @@
 import Foundation
 
 enum PreviewData {
-  static let events: [Event] = [
-    Event(
-      id: "1",
-      title: "南知读书会第一期",
-      time: "3月25日 周一 14:00-16:00",
-      location: "知识星球",
-      isRecurring: true,
-      calendar: "工作"
-    ),
-    Event(
-      id: "2",
-      title: "产品评审会",
-      time: "3月26日 周二 10:00-11:30",
-      location: "腾讯会议",
-      isRecurring: false,
-      calendar: "工作"
-    ),
-    Event(
-      id: "3",
-      title: "团队周会",
-      time: "3月27日 周三 09:30-10:30",
-      location: "会议室A",
-      isRecurring: true,
-      calendar: "工作"
-    ),
-  ]
-  
+
   static let mockCalendarEvents: [CalendarEvent] = [
     CalendarEvent(
       title: "南知读书会第一期",
@@ -75,9 +49,4 @@ class MockLLMProcessor: LLMEventProcessor {
   }
 }
 
-extension Event {
-  static var preview: Event {
-    PreviewData.events[0]
-  }
-}
 #endif
