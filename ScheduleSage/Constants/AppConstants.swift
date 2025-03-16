96d5cbbc1e22a61182e7bf6301f1d85d7c9ba331
南知 <deer_hope@live.cn>
2025-03-16 17:05:27 +0800

    更新文档链接，notion.size在不加速的情况下，无法访问

--- ScheduleSage/Constants/AppConstants.swift
+++ ScheduleSage/Constants/AppConstants.swift
@@ -3,9 +3,9 @@ import Foundation
 enum AppConstants {
     enum URLs {
         static let feedback = "https://schedulesage.featurebase.app/"
-        static let privacyPolicy = "https://tiwenlab.notion.site/18f5180108e580f69c59f212867f9a15"
-        static let userAgreement = "https://tiwenlab.notion.site/18f5180108e5804cb596c845db6754bd"
-        static let faq = "https://tiwenlab.notion.site/FAQ-18f5180108e58034aecdec8a297c97ab?pvs=74"
+        static let privacyPolicy = "https://docs.qq.com/doc/DVEViT2NraHNJcE5R"
+        static let userAgreement = "https://docs.qq.com/doc/DVERMWWhidHJwbVJy"
+        static let faq = "https://schedulesage.featurebase.app/help"
         static let sentryUrl = "https://8dbe56154a591426c2ecb7ed66018a1a@o4508634309066752.ingest.us.sentry.io/4508822914793472"
         static let cancelSubscription = "https://support.apple.com/zh-cn/118428"
     }
